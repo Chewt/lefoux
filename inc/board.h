3e13b6c91293669b4aa1f185923e98632e5331d5
Hayden Johnson <hajohn100@gmail.com>
2023-05-08 21:57:04 -0700

    Add type indicators to macros in board.h
    Added leading character hints to what types macros work on. For example
    getenp() became bgetenp() because it works on a Board struct, and
    getweight() became mgetweight() because it works on a Move type.

--- inc/board.h
+++ inc/board.h
@@ -26,14 +26,13 @@ typedef struct
 } Board;
 
 // Extract en passant from info
-#define getenp(x) (0x3F & (x >> 5))
+#define bgetenp(x) ((uint8_t)(0x3F & (x >> 5)))
 
 // Extract castling from info
-#define getcas(x) (0x0F & (x >> 1))
+#define bgetcas(x) ((uint8_t)(0x0F & (x >> 1)))
 
 // Extract color to play from info.
-// Also used to extract color from Move type below
-#define getcol(x) (0x01 & x)
+#define bgetcol(x) ((uint8_t)(0x01 & x))
 
 
 /******************************************************************************
@@ -41,7 +40,7 @@ typedef struct
  * 
  * Information is laid out like so:
  *
- * | Weight        | Source      | Destination | Piece   | Promotion | Color
+ * | Weight        | Source      | Destination | Piece   | Promote | Color
  * +-----------------------------------------------------------------------+
  * | 8 bits        | 6 bits      | 6 bits      | 3 bit   | 3 bit   | 1 bit |  
  * +-----------------------------------------------------------------------+
@@ -52,25 +51,26 @@ typedef struct
  * - Destination is the destination of the piece that will move
  * - Piece is the type of piece that is moving
  * - Color is the color of the piece that is moving
- * - Promotion is the piece that a pawn will promote to
+ * - Promote is the piece that a pawn will promote to
  *****************************************************************************/
 typedef uint32_t Move;
 
 // Extract move weight
-#define getweight(x) ((int8_t)(0xFF & (x >> 19)))
+#define mgetweight(x) ((int8_t)(0xFF & (x >> 19)))
 
 // Extract source location from move
-#define getsrc(x)    ((uint8_t)(0x3F & (x >> 13)))
+#define mgetsrc(x)    ((uint8_t)(0x3F & (x >> 13)))
 
 // Extract destination location from move
-#define getdst(x)    ((uint8_t)(0x3F & (x >>  7)))
+#define mgetdst(x)    ((uint8_t)(0x3F & (x >>  7)))
 
 // Extract piece type from move
-#define getpiece(x)  ((uint8_t)(0x07 & (x >>  4)))
+#define mgetpiece(x)  ((uint8_t)(0x07 & (x >>  4)))
 
 // Extract promotion piece from move
-#define getprom(x)   ((uint8_t)(0x07 & (x >>  1)))
+#define mgetprom(x)   ((uint8_t)(0x07 & (x >>  1)))
 
-// getcol(x) defined earlier in file works on move type as well
+// Extract color to play from move.
+#define mgetcol(x)    ((uint8_t)(0x01 & x))
 
 #endif /* end of include guard: BOARD_H */
