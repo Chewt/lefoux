8cd0729cc7ea67e1cca41d67692f3b7117984e75
Hayden Johnson <hajohn100@gmail.com>
2023-09-16 14:06:42 -0700

    Add basic argparsing
    I added the framework for command line argument parsing, and to start
    with just one flag that can be set. running "./lefoux --test" will run
    all of the unit tests at the beginning of the program. It is fairly
    straightforward to add more arguments as we need them.

--- src/main.c
+++ src/main.c
@@ -1,13 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <omp.h>
+#include <argp.h>
 
 #include "bitHelpers.h"
 #define NUM_THREADS 1
 
 void tests();
 
-int main()
+/* argp struct */
+struct flags {
+
+};
+
+const char *argp_program_bug_address = "https://github.com/Chewt/lefoux/issues";
+#ifdef VERSION
+const char *argp_program_version = VERSION;
+#endif
+
+/* Parse Arguments */
+static int parse_opt (int key, char *arg, struct argp_state *state)
 {
+    struct flags* flags = state->input;
+    switch(key)
+    {
+        case 500:
+            tests();
+            break;
+    }
+    return 0;
+}
+
+int main(int argc, char** argv)
+{
+    /* Command line args */
+    struct flags flags;
+    struct argp_option options[] = {
+        {"test", 500, 0, 0, "Run unit tests"},
+        { 0 }
+    };
+    struct argp argp = {options, parse_opt, 0, "Multithreaded chess engine.",
+                        0,       0,         0};
+    if (argp_parse(&argp, argc, argv, 0, 0, &flags))
+    {
+        fprintf(stderr, "Error parsing arguments\n");
+        exit(-1);
+    }
+
 #ifdef _OPENMP
 	fprintf( stderr, "OpenMP is supported -- version = %d\n", _OPENMP );
 #else
