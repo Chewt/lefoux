0acd8d02f7924544873240e6c485406fdd00af83
Hayden Johnson <hajohn100@gmail.com>
2023-09-17 21:52:12 -0700

    Add comments to uci files
    Also changed the "exit" command to "quit" in accordance with the UCI
    specification.

--- src/uci.c
+++ src/uci.c
@@ -4,15 +4,42 @@
 #include "uci.h"
 #include "board.h"
 
-void uciReady(Board* board, char* command)
+/*******************************************************************************
+ *
+ * Command functions
+ *
+ * These functions must follow all follow the same pattern and function
+ * signature. They must return an int, and take two arguments: a Board pointer
+ * and a char pointer. The Board pointer is the current boardstate, and the
+ * char pointer is the entire command that is being processed. The name of the
+ * function corresponds to the command name.
+ *
+ ******************************************************************************/
+
+int isready(Board* board, char* command)
 {
     char* s = "readyok\n";
     if (write(1, s, strlen(s)) == -1)
         printf("WHAT?!? there was an error\n");
+    return 1;
 }
 
+int uci(Board* board, char* command)
+{
+    char* s = "id name lefoux\nid author Hayden Johnson, Zach Gorman\nuciok\n";
+    if (write(1, s, strlen(s)) == -1)
+        printf("WHAT?!? there was an error\n");
+    return 1;
+}
+
+/* This struct holds all of the commands that are accepted by lefoux. The format
+ * for this struct is a keyword to match, that is the first word in the command,
+ * and a function pointer to be called if the command matches the keyword. The
+ * last entry is filled with zeros to mark the end of the array.
+ */
 Command allcommands[] = {
-    {"isready", uciReady},
+    {"isready", isready},
+    {"uci", uci},
     { 0 }
 };
 
@@ -26,13 +53,10 @@ int ProcessCommand(Board* board, char* command)
     Command* c;
     for (c = allcommands; c->match[0] != 0; c++)
     {
-        if (!strcmp("exit", token))
+        if (!strcmp("quit", token))
             return 0;
         if (!strcmp(c->match, token))
-        {
-            c->func(board, command_copy);
-            break;
-        }
+            return c->func(board, command_copy);
     }
     return 1;
 }
